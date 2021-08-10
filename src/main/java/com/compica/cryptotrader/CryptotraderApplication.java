41267aa0775fcf9fb175377421d5d5c68b287bd4
Martin Fournier <mfournier@rheinmetall.ca>
2021-08-10 12:51:29 -0400

    initial commit

---
+++ src/main/java/com/compica/cryptotrader/CryptotraderApplication.java
@@ -0,0 +1,13 @@
+package com.compica.cryptotrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compica/cryptotrader/CryptotraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compica.cryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
